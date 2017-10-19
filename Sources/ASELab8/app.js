0ed53626d8b8b1d899b4a731a4bbf82ef6592a9c
Wayne Nguyen <wayneie_n@hotmail.com>
2017-10-19 08:10:10 -0500

    screenshots

--- Sources/ASELab8/app.js
+++ Sources/ASELab8/app.js
@@ -1,5 +1,26 @@
-var app = angular.module("lab9app",[]);
+var app=angular.module("lab9app",[]);
+app.controller("lab9controller",function ($scope, $http) {
+    $scope.gone = function () {
+        var goto=$scope.gkeyword+'&'+$scope.lang;
 
-app.controller("googleknowledge", function ($scope, $http) {
-    
-})
+        console.log(goto);
+
+        var words = $http.get("http://127.0.0.1:8081/data/"+goto);
+        words.success(function (data) {
+            console.log(data[0].PixabayImage);
+
+            $scope.gresults2={
+                "name":data[0].name,
+                "description":data[0].description,
+                "translatedDescription":data[0].translatedDescription,
+                "translatedName":data[0].translatedName,
+                "PixabayImage":data[0].PixabayImage
+            };
+        });
+    }
+});
+
+
+function changeLanguage(text) {
+    localStorage.setItem('lang',text);
+}
--- Sources/ASELab8/server.js
+++ Sources/ASELab8/server.js
@@ -3,14 +3,23 @@ var app = express();
 var request = require('request');
 var http = require('http'), https = require('https');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-
-var result ={'gresult':[]};
-var result1 ={'yresult':[]};
-var ven2;
-app.get('/data', function (req,res)
+app.get('/data/:id', function (req,res)
 {
-    request('https://kgsearch.googleapis.com/v1/entities:search?query=ball&key=AIzaSyAIJC4iA7279Vyq2gLVfzzDmZUooqShUhI&limit=10',
+    var result1= {'yresult':[]};
+    var result2 = {'y2result':[]};
+    var result ={'gresult':[]};
+    var parts= req.params.id.split('&', 2);
+    var search = parts[0];
+    var lang= parts[1];
+
+
+    request('https://kgsearch.googleapis.com/v1/entities:search?query='+search+'&key=AIzaSyAIJC4iA7279Vyq2gLVfzzDmZUooqShUhI&limit=1',
         function (error, response, body) {
             if (error) {
                 return console.log('Google Api Error', error);
@@ -23,73 +32,96 @@ app.get('/data', function (req,res)
             var ven = body.itemListElement;
             //console.log(ven);
 
+            var replacedName = ven[0].result.name.replace(/ /g, "%20");
+            var replacedDesc = ven[0].result.description.replace(/ /g, "%20");
+            var Name = ven[0].result.name;
+            console.log(ven[0].result.name);
+            var Description = ven[0].result.description;
+            console.log(ven[0].result.description);
             var options = {
                 host: 'translate.yandex.net',
                 port: 443,
-                path: '/api/v1.5/tr.json/translate?key=trnsl.1.1.20170913T034651Z.489eb6aa12589e99.525b452bb54efb9faac7e0307e28d7d024576cf1&lang=fr&text=ball',
+                path: '/api/v1.5/tr.json/translate?key=trnsl.1.1.20170913T034651Z.489eb6aa12589e99.525b452bb54efb9faac7e0307e28d7d024576cf1&lang=fr&text=' + replacedName,
                 method: 'GET'
             };
 
-            https.request(options, function (res) {
+            https.request(options, function (res1) {
 
-                res.setEncoding('utf8');
+                res1.setEncoding('utf8');
 
-                res.on('data', function (chunk) {
-                    var res = JSON.parse(chunk);
-                    if (res.code == 200) {
-                        ven2 = res.text;
-
-                        console.log(res.text + '');
+                res1.on('data', function (chunk) {
+                    var rest = null;
+                    rest = JSON.parse(chunk);
+                    if (rest.code == 200) {
+                        console.log(rest.text + '');
                     } else {
                         console.log('ERR ' + chunk);
                     }
+                    result1.yresult.push(rest.text);
 
                 });
 
+
+
+                var options2 = {
+                    host: 'translate.yandex.net',
+                    port: 443,
+                    path: '/api/v1.5/tr.json/translate?key=trnsl.1.1.20170913T034651Z.489eb6aa12589e99.525b452bb54efb9faac7e0307e28d7d024576cf1&lang='+lang+'&text=' + replacedDesc,
+                    method: 'GET'
+                };
+
+                https.request(options2, function (res2) {
+
+                    res2.setEncoding('utf8');
+
+                    res2.on('data', function (chunk) {
+                        var rest1 = null;
+                        rest1 = JSON.parse(chunk);
+                        if (rest1.code == 200) {
+                            console.log(rest1.text + '');
+                        } else {
+                            console.log('ERR ' + chunk);
+                        }
+                        result2.y2result.push(rest1.text);
+
+                    });
+
+
+                    request('https://pixabay.com/api/?key=6627635-091e9de4bc6b9740a958fffa2&q='+replacedName,
+                        function (error, response, body) {
+                            if (error) {
+                                return console.log('Google Api Error', error);
+                            }
+                            if (response.statusCode !== 200) {
+                                return console.log('Invalid code returned', response.statusCode)
+                            }
+                            body = JSON.parse(body);
+
+                            pix= body.hits;
+                            console.log(pix[0].previewURL);
+
+                            result.gresult.push({
+                                'name': Name,
+                                'description': Description,
+                                'translatedDescription': result2.y2result[0].toString(),
+                                'translatedName': result1.yresult[0].toString(),
+                                'PixabayImage': pix[0].previewURL
+                            });
+
+                            res.contentType('application/json');
+                            res.write(JSON.stringify(result.gresult));
+                            res.end();
+
+                        });
+                }).end();
             }).end();
-
-            console.log(ven2);
-            for (var i = 0; i < ven.length; i++) {
-                result.gresult.push({'name': ven[i].result.name, 'description': ven[i].result.description,'translated':ven2});
-            }
-            res.contentType('application/json');
-            res.write(JSON.stringify(result));
-            res.end();
-
         });
-
-});
-
-/*app.get('/data2', function (req,res) {
-
-        var options = {
-            host: 'translate.yandex.net',
-            port: 443,
-            path: '/api/v1.5/tr.json/translate?key=trnsl.1.1.20170913T034651Z.489eb6aa12589e99.525b452bb54efb9faac7e0307e28d7d024576cf1&lang=fr&text=flower',
-            method: 'GET'
-        };
-
-        https.request(options, function (res) {
-
-            res.setEncoding('utf8');
-
-            res.on('data', function (chunk) {
-                var res = JSON.parse(chunk);
-                if (res.code == 200) {
-                    result1.yresult.push(res.text);
-                    console.log(res.text + '');
-                } else {
-                    console.log('ERR ' + chunk);
-                }
-
-            });
-        }).end();
-});*/
+    });
 
 
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("http://127.0.0.1:8081/data");
-    console.log("http://127.0.0.1:8081/data2");
+    console.log("http://127.0.0.1:8081/data/");
+
 });
